c9f2ade6c76d8beaf3a7044fbd69a5aa401edc51
VicKon <69324970+vickon16@users.noreply.github.com>
2024-03-30 14:13:23 +0000

    Updated application

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Providers from "@/providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="p-4">{children}</main>
+        <Providers>
+          <main className="p-4">{children}</main>
+        </Providers>
       </body>
     </html>
   );
